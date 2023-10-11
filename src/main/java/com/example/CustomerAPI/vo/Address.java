4dd1b837295c8ac0e2fc14ced0573e2641271f5b
Sinan Özbek <sinan.ozbek@arbetsformedlingen.se>
2023-10-11 21:22:49 +0200

    added Schemas for documentation purposes

--- src/main/java/com/example/CustomerAPI/vo/Address.java
+++ src/main/java/com/example/CustomerAPI/vo/Address.java
@@ -1,6 +1,7 @@
 package com.example.CustomerAPI.vo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.extern.jackson.Jacksonized;
 
@@ -8,8 +9,17 @@ import lombok.extern.jackson.Jacksonized;
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Address(
+        @Schema(description = "city of the customer",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "City")
         String city,
+        @Schema(description = "postcode of the customer",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "PostCode")
         String postCode,
+        @Schema(description = "Street of the customer",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "Street")
         String street
 
 ) {
--- src/main/java/com/example/CustomerAPI/vo/Customer.java
+++ src/main/java/com/example/CustomerAPI/vo/Customer.java
@@ -1,6 +1,7 @@
 package com.example.CustomerAPI.vo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.extern.jackson.Jacksonized;
 import org.springframework.data.annotation.Id;
@@ -12,11 +13,29 @@ import java.sql.Timestamp;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Customer(
         @Id
+        @Schema(description = "Id of the customer",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "Id")
         String personOfficialId,
+        @Schema(description = "Address of the customer",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "Adrress")
         Address address,
+        @Schema(description = "First name of the customer",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "First name")
         String firstName,
+        @Schema(description = "Last name of the customer",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "Last name")
         String lastName,
+        @Schema(description = "The last time the customer object was updated",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "Last Updated")
         Timestamp lastUpdated,
+        @Schema(description = "If the person is of type Legal or Private",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                example = "personType")
         String personType
 ) {
 }
