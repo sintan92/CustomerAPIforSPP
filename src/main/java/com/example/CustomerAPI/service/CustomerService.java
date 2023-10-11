62e177ff517ec2751eaef1f878432a44208b7e61
Sinan Özbek <sinan.ozbek@arbetsformedlingen.se>
2023-10-11 18:32:51 +0200

    cleanup

--- src/main/java/com/example/CustomerAPI/service/CustomerService.java
+++ src/main/java/com/example/CustomerAPI/service/CustomerService.java
@@ -5,9 +5,6 @@ import com.example.CustomerAPI.restout.CustomerClient;
 import com.example.CustomerAPI.vo.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.UUID;
 
 @Service
 public class CustomerService{
--- src/main/java/com/example/CustomerAPI/vo/Customer.java
+++ src/main/java/com/example/CustomerAPI/vo/Customer.java
@@ -1,9 +1,6 @@
 package com.example.CustomerAPI.vo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import lombok.Builder;
 import lombok.extern.jackson.Jacksonized;
 import org.springframework.data.annotation.Id;
@@ -15,13 +12,10 @@ import java.sql.Timestamp;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Customer(
         @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
         String personOfficialId,
         Address address,
         String firstName,
         String lastName,
-
-        @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
         Timestamp lastUpdated,
         String personType
 ) {
